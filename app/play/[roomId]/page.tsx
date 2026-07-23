a055689ea82f9042ad268883a5183bc338637d34
Abdoubel26 <abdoubelounis09@gmail.com>
2026-07-23 16:40:21 +0100

    add dynamic backend url and delete drizzle.config

--- app/play/[roomId]/page.tsx
+++ app/play/[roomId]/page.tsx
@@ -54,7 +54,9 @@ export default function GameRoom() {
   useEffect(() => {
     if (!roomId) return;
 
-    const socket = io("http://localhost:5000");
+    const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000";
+
+    const socket = io(BACKEND_URL);
     socketRef.current = socket;
 
     socket.on("connect", () => {
